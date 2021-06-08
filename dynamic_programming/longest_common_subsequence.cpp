dbe72554362dafbc65c88474af2f5c9312fb0566
manncodes <manncodes@gmail.com>
2021-06-08 12:38:02 +0530

    Fixed namespacing, added test and stress test

--- dynamic_programming/longest_common_subsequence.cpp
+++ dynamic_programming/longest_common_subsequence.cpp
@@ -1,112 +1,65 @@
-/**
- * @file
- * @brief Calculate the length of the [longest increasing
- * subsequence](https://en.wikipedia.org/wiki/Longest_increasing_subsequence) in
- * an array
- *
- * @details
- * In computer science, the longest increasing subsequence problem is to find a
- * subsequence of a given sequence in which the subsequence's elements are in
- * sorted order, lowest to highest, and in which the subsequence is as long as
- * possible. This subsequence is not necessarily contiguous, or unique. Longest
- * increasing subsequences are studied in the context of various disciplines
- * related to mathematics, including algorithmics, random matrix theory,
- * representation theory, and physics. The longest increasing subsequence
- * problem is solvable in time O(n log n), where n denotes the length of the
- * input sequence.
- *
- * @author [Krishna Vedala](https://github.com/kvedala)
- * @author [David Leal](https://github.com/Panquesito7)
- * @author [Mann Patel](https://github.com/manncodes)
- */
+// Longest common subsequence - Dynamic Programming
+#include <iostream>
+using namespace std;
 
-
-#include <cassert>   /// for assert
-#include <climits>   /// for std::max
-#include <iostream>  /// for IO operations
-#include <vector>    /// for std::vector
-
-/**
- * @brief Calculate the longest increasing subsequence for the specified numbers
- * @param a the array used to calculate the longest increasing subsequence
- * @param n the size used for the arrays
- * @returns the length of the longest increasing
- * subsequence in the `a` array of size `n`
- */
-
-/**
- * @namespace dynamic_programming
- * @brief Dynamic Programming algorithms
- */
-namespace dynamic_programming {
-/**
- * @namespace Knapsack
- * @brief Implementation of 0-1 Knapsack problem
- */
-namespace longest_increasing_subsequence {
-
-int LIS(const std::vector<uint64_t> &a, const uint32_t &n) {
-    std::vector<int> lis(n);
-    for (int i = 0; i < n; ++i) {
-        lis[i] = 1;
+void Print(int trace[20][20], int m, int n, string a) {
+    if (m == 0 || n == 0) {
+        return;
     }
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < i; ++j) {
-            if (a[i] > a[j] && lis[i] < lis[j] + 1) {
-                lis[i] = lis[j] + 1;
+    if (trace[m][n] == 1) {
+        Print(trace, m - 1, n - 1, a);
+        cout << a[m - 1];
+    } else if (trace[m][n] == 2) {
+        Print(trace, m - 1, n, a);
+    } else if (trace[m][n] == 3) {
+        Print(trace, m, n - 1, a);
+    }
+}
+
+int lcs(string a, string b) {
+    int m = a.length(), n = b.length();
+    int res[m + 1][n + 1];
+    int trace[20][20];
+
+    // fills up the arrays with zeros.
+    for (int i = 0; i < m + 1; i++) {
+        for (int j = 0; j < n + 1; j++) {
+            res[i][j] = 0;
+            trace[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < m + 1; ++i) {
+        for (int j = 0; j < n + 1; ++j) {
+            if (i == 0 || j == 0) {
+                res[i][j] = 0;
+                trace[i][j] = 0;
+            }
+
+            else if (a[i - 1] == b[j - 1]) {
+                res[i][j] = 1 + res[i - 1][j - 1];
+                trace[i][j] = 1;  // 1 means trace the matrix in upper left
+                                  // diagonal direction.
+            } else {
+                if (res[i - 1][j] > res[i][j - 1]) {
+                    res[i][j] = res[i - 1][j];
+                    trace[i][j] =
+                        2;  // 2 means trace the matrix in upwards direction.
+                } else {
+                    res[i][j] = res[i][j - 1];
+                    trace[i][j] =
+                        3;  //  means trace the matrix in left direction.
+                }
             }
         }
     }
-    int res = 0;
-    for (int i = 0; i < n; ++i) {
-        res = std::max(res, lis[i]);
-    }
-    return res;
+    Print(trace, m, n, a);
+    return res[m][n];
 }
 
-} // namespace longest_increasing_subsequence
-} // namespace dynamic_programming
-
-
-/**
- * @brief Function to stress test above algorithm n=30,000
- * @return void
- */
-void stresstest(){
-    int answer = 0;
-    freopen("stress_test_file.txt", "r", stdin);
-    uint32_t n;
-    std::cin >> n;
-    std::vector<uint64_t> a(n);
-    for(auto&i: a){
-        std::cin >> i;
-    }
-    int result = dynamic_programming::longest_increasing_subsequence::LIS(a, a.size());
-    std::cin >> answer;
-    assert(result == answer);
-    std::cout <<"Stress test implementation passed!"<< std::endl;
-}
-
-/**
- * @brief Function to test above algorithm
- * @returns void
- */
-void test(){
-    std::vector<uint64_t> a = {15,21,2,3,4,5,8,4,1,1};
-    uint32_t n  = a.size();
-    int result = dynamic_programming::longest_increasing_subsequence::LIS(a, n);
-    assert(result == 5);  ///< Longest  increasing subsequence is {2,3,4,5,8}.
-    std::cout << "Test implementation passed!" << std::endl;
-}
-
-/**
- * @brief Main function
- * @param argc commandline argument count (ignored)
- * @param argv commandline array of arguments (ignored)
- * @returns 0 on exit
- */
-int main(int argc, char const *argv[]) {
-    test();
-    stresstest();
+int main() {
+    string a, b;
+    cin >> a >> b;
+    cout << lcs(a, b);
     return 0;
 }
--- dynamic_programming/longest_increasing_subsequence.cpp
+++ dynamic_programming/longest_increasing_subsequence.cpp
@@ -1,32 +1,112 @@
-// Program to calculate length of longest increasing subsequence in an array
-#include <bits/stdc++.h>
-using namespace std;
-int LIS(int a[], int n) {
-    int lis[n];
+/**
+ * @file
+ * @brief Calculate the length of the [longest increasing
+ * subsequence](https://en.wikipedia.org/wiki/Longest_increasing_subsequence) in
+ * an array
+ *
+ * @details
+ * In computer science, the longest increasing subsequence problem is to find a
+ * subsequence of a given sequence in which the subsequence's elements are in
+ * sorted order, lowest to highest, and in which the subsequence is as long as
+ * possible. This subsequence is not necessarily contiguous, or unique. Longest
+ * increasing subsequences are studied in the context of various disciplines
+ * related to mathematics, including algorithmics, random matrix theory,
+ * representation theory, and physics. The longest increasing subsequence
+ * problem is solvable in time O(n log n), where n denotes the length of the
+ * input sequence.
+ *
+ * @author [Krishna Vedala](https://github.com/kvedala)
+ * @author [David Leal](https://github.com/Panquesito7)
+ * @author [Mann Patel](https://github.com/manncodes)
+ */
+
+
+#include <cassert>   /// for assert
+#include <climits>   /// for std::max
+#include <iostream>  /// for IO operations
+#include <vector>    /// for std::vector
+
+/**
+ * @brief Calculate the longest increasing subsequence for the specified numbers
+ * @param a the array used to calculate the longest increasing subsequence
+ * @param n the size used for the arrays
+ * @returns the length of the longest increasing
+ * subsequence in the `a` array of size `n`
+ */
+
+/**
+ * @namespace dynamic_programming
+ * @brief Dynamic Programming algorithms
+ */
+namespace dynamic_programming {
+/**
+ * @namespace Knapsack
+ * @brief Implementation of 0-1 Knapsack problem
+ */
+namespace longest_increasing_subsequence {
+
+int LIS(const std::vector<uint64_t> &a, const uint32_t &n) {
+    std::vector<int> lis(n);
     for (int i = 0; i < n; ++i) {
         lis[i] = 1;
     }
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < i; ++j) {
-            if (a[i] > a[j] && lis[i] < lis[j] + 1)
+            if (a[i] > a[j] && lis[i] < lis[j] + 1) {
                 lis[i] = lis[j] + 1;
+            }
         }
     }
     int res = 0;
     for (int i = 0; i < n; ++i) {
-        res = max(res, lis[i]);
+        res = std::max(res, lis[i]);
     }
     return res;
 }
-int main(int argc, char const *argv[]) {
-    int n;
-    cout << "Enter size of array: ";
-    cin >> n;
-    int a[n];
-    cout << "Enter array elements: ";
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
+
+} // namespace longest_increasing_subsequence
+} // namespace dynamic_programming
+
+
+/**
+ * @brief Function to stress test above algorithm n=30,000
+ * @return void
+ */
+void stresstest(){
+    int answer = 0;
+    freopen("stress_test_file.txt", "r", stdin);
+    uint32_t n;
+    std::cin >> n;
+    std::vector<uint64_t> a(n);
+    for(auto&i: a){
+        std::cin >> i;
     }
-    cout << LIS(a, n) << endl;
+    int result = dynamic_programming::longest_increasing_subsequence::LIS(a, a.size());
+    std::cin >> answer;
+    assert(result == answer);
+    std::cout <<"Stress test implementation passed!"<< std::endl;
+}
+
+/**
+ * @brief Function to test above algorithm
+ * @returns void
+ */
+void test(){
+    std::vector<uint64_t> a = {15,21,2,3,4,5,8,4,1,1};
+    uint32_t n  = a.size();
+    int result = dynamic_programming::longest_increasing_subsequence::LIS(a, n);
+    assert(result == 5);  ///< Longest  increasing subsequence is {2,3,4,5,8}.
+    std::cout << "Test implementation passed!" << std::endl;
+}
+
+/**
+ * @brief Main function
+ * @param argc commandline argument count (ignored)
+ * @param argv commandline array of arguments (ignored)
+ * @returns 0 on exit
+ */
+int main(int argc, char const *argv[]) {
+    test();
+    stresstest();
     return 0;
 }
